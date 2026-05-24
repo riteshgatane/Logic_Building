b52d681b79c181a22a52f851aa24930debb6672a
ritesh_gatane <riteshgatane2006@gmail.com>
2026-05-24 23:29:37 +0530

    Add files via upload
    Check Even or Odd

---
+++ program16.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckEvenOdd(int iNo)
+{
+
+    if((iNo % 2)  == 0)   //
+    {
+        return true;
+    }
+    else
+    {
+        return false ; 
+    }
+}
+    
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+   
+
+    printf("Enter Number to check the umber is even or odd:");
+    scanf("%d",&iValue);
+
+    bRet =CheckEvenOdd(iValue);
+    if(bRet == true) 
+    {
+        printf("%d is Even ",iValue);
+    }
+    else 
+    {
+        printf("%d is Odd",iValue);
+    }
+
+    return 0 ;
+}
