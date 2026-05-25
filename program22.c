67b9943d10a5f80ec9fd15c13a9eb78d4063cffa
ritesh_gatane <riteshgatane2006@gmail.com>
2026-05-25 19:00:45 +0530

    Add files via upload

---
+++ program22.c
@@ -0,0 +1,50 @@
+#include<stdio.h>  
+
+int CalculateTicketPrice(int iAge)
+{   
+      ///Input Filter
+    if (iAge < 0 )
+    {
+        
+        return -1;
+    }
+    if(iAge >=0 && iAge <=5)
+    {
+        return 0;
+    }
+    else if(iAge >=6 && iAge <=18)
+    {
+        return 500 ;
+    }
+
+    else if(iAge >=19 && iAge <= 50 )
+    {
+        return 900;
+    }
+    else
+    {
+        return 400;
+    }
+
+
+
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+    printf("Please enter your Age to Calculate Ticket Price:");
+    scanf("%d",&iValue);
+
+    iRet = CalculateTicketPrice(iValue);
+
+    if(iRet == -1)
+    {
+        printf("Please enter positive Age");
+    }
+    else{
+    printf("Your Ticket price will be %d Ruppes",iRet);
+    }
+    return 0 ;
+}
