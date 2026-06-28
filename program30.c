de492a02e0b2c8ca23d52293dbbd0d6e95e8ae49
ritesh_gatane <riteshgatane2006@gmail.com>
2026-06-29 02:14:09 +0530

    Add files via upload

---
+++ program30.c
@@ -0,0 +1,32 @@
+ #include<stdio.h>  
+
+void Display(int iNo)
+{
+   int iCnt = 0 ;
+   
+   if(iNo < 0)
+   {  //Filter
+      iNo = -iNo;
+      printf("Invalid input ");
+      return ;
+   }
+ 
+    for(iCnt=1;iCnt<=iNo; iCnt++)
+      {
+         printf("Jay Ganesh\n");
+      }
+
+}
+int main()
+{
+
+   
+   int iValue = 0;
+   printf("Enter the Frequnecy:");
+   scanf("%d",&iValue);
+
+   Display(iValue);
+  
+
+   return 0 ;
+}
