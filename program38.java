3cdc8786e42ea698d75fe13d837cd1d0d3f6f92b
ritesh_gatane <riteshgatane2006@gmail.com>
2026-07-11 12:40:03 +0530

    Add files via upload

---
+++ program38.java
@@ -0,0 +1,22 @@
+
+import  java.util.*;
+class program38
+{
+    public static void main(String A[])
+    {
+        int iCnt = 0 ;
+
+        for(iCnt = 1 ; iCnt <=10 ; iCnt++)
+        {
+            System.out.printf("%d\n",+iCnt); ///printf
+        }
+        
+    }
+}
+
+
+
+/////time complexity - Algorithm time taken to execute the loop only for thw loop if thier is no loop time comple will not thier 
+///space cokmplexity -  Dynamic memory 
+///1)run the code 
+///2)reduce the complexity   
