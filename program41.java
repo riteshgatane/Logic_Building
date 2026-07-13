6dda4aa2752a0423a8596014113bd912fa58e419
ritesh_gatane <riteshgatane2006@gmail.com>
2026-07-13 17:09:34 +0530

    Add files via upload

---
+++ program41.java
@@ -0,0 +1,34 @@
+// TYPE 2
+
+
+
+import  java.util.Scanner;
+
+class program41
+{
+    static void  CheckDivisible(int iNo )    // In c BOOL 
+    {
+        if((iNo % 3 == 0 )&& (iNo % 5 == 0))
+        {
+            System.out.println("Number is DIvision by 3 And 5");
+        }
+        else
+        {
+           System.out.println("Number is not  DIvision by 3 And 5");
+        }
+        
+
+    }
+    public static void main(String A[])
+    {
+        int iValue= 0 ;
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number :");
+        iValue = sobj.nextInt();
+
+        CheckDivisible(iValue);    
+        
+    }
+}
+
