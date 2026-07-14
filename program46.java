b8a6dd805e9137ac0044b17ae0df5b6ca699466d
ritesh_gatane <riteshgatane2006@gmail.com>
2026-07-14 23:05:25 +0530

    Add files via upload

---
+++ program46.java
@@ -0,0 +1,41 @@
+
+import  java.util.Scanner;
+
+class NumberX
+{
+    public  void DisplayFactors(int iNo)
+    {  ///logic 
+        int iCnt = 0 ;
+        for (iCnt = 1; iCnt< iNo ; iCnt++) 
+        { if ((iNo % iCnt) == 0)
+            {
+                System.out.println(iCnt);
+            }
+        }
+        
+    }
+    
+
+}
+
+class program46
+{  
+    public static void main(String A[])
+    {
+        int iValue= 0 ;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number :");
+        iValue = sobj.nextInt();
+
+
+        NumberX nobj = new NumberX();
+        nobj.DisplayFactors(iValue);
+       
+        
+    }
+}
+
+//TIme Complexity : O(N)
+//Where N>= 0
