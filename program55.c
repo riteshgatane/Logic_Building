f139fae85e06e4c4e3af272b88388e3981ee0181
ritesh_gatane <riteshgatane2006@gmail.com>
2026-08-11 01:23:36 +0530

    Add files via upload

---
+++ program55.c
@@ -0,0 +1,36 @@
+///iNput : 8
+//Output: 1 3 5 7
+
+
+#include <stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt=1 ; iCnt <iNo ; iCnt = iCnt + 2)  //internally iCnt is iCnt = iCnt + 1  
+    
+    {
+       
+        
+        printf("%d\t",iCnt);
+
+    }
+
+    printf("\n");
+    
+}
+
+int main()
+{
+
+    int iValue =0 ;
+    printf("Enter the number :");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+
+
+    return 0 ;
+}
