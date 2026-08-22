05e118c141604386def3310bffb60695b2c7e023
ritesh_gatane <riteshgatane2006@gmail.com>
2026-08-22 23:45:15 +0530

    Add files via upload

---
+++ program75.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>///
+int main()
+{
+    
+    int iNo = 751;
+    int iDigit = 0 ;
+
+    while (iNo != 0 )
+    {                               //751 
+        iDigit = iNo % 10; 
+        printf("%d\t",iDigit);
+        iNo = iNo/10 ;
+
+        iDigit = iNo % 10; 
+        printf("%d\t",iDigit);
+        iNo = iNo/10 ;
+
+        iDigit = iNo % 10; 
+        printf("%d\t",iDigit);
+        iNo = iNo/10 ;
+
+    }
+    printf("\n");
+    
+
+    
+
+    return 0 ;
+}
