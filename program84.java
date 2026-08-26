0fcdf486ce7165d1b5c37e023e0a9c8dd09704ee
ritesh_gatane <riteshgatane2006@gmail.com>
2026-08-26 09:22:23 +0530

    Add files via upload

---
+++ program84.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class DigitX
+{
+    public int CountDigits(int iNo)
+    {
+       
+        int iCount = 0 ;
+
+        while(iNo != 0 )
+        {        
+          iCount++ ; 
+          iNo = iNo/10 ;
+        
+        }
+        return  iCount ;
+    }
+}
+
+class program84
+{
+    public static void main(String A[])
+    {
+        int iValue= 0 ;
+        int iRet = 0 ;
+        Scanner sobj = new Scanner(System.in);
+        DigitX dobj = new DigitX();
+
+        System.out.println("Enter the Number :");
+
+        iValue = sobj.nextInt();
+
+        iRet = dobj.CountDigits(iValue);
+        System.out.println("Number of Digits are:"+iRet);
+    
+    }
+}
