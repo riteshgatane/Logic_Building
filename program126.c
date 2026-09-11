3a6b04c2f5391653c000b35837efe56d0a301c8e
Ritesh Gatane <254677979+riteshgatane@users.noreply.github.com>
2026-09-11 14:18:25 +0530

    Add files via upload

---
+++ program126.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int SumOdd (int Arr[], int iSize)
+{
+    int iSum = 0;
+    int iCnt = 0;
+    for (iCnt = 0 ; iCnt < iSize ; iCnt ++)
+   {
+     if ((Arr[iCnt]% 2) != 0)
+    {
+       iSum = iSum + Arr[iCnt];
+    }
+    }
+    
+
+    return iSum ;
+    
+}
+
+int main()
+{
+    int *Brr = NULL ;
+    int iLength = 0,iCnt = 0  ;
+    int iRet = 0 ;
+    
+    printf("Enter the Number of elements:");
+    scanf("%d",&iLength);
+
+  
+    Brr = (int *)malloc(iLength * sizeof(int ));
+
+  
+    printf("Enter the elements:\n");
+    for (iCnt = 0 ; iCnt < iLength ; iCnt ++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    iRet = SumOdd(Brr , iLength);
+    printf("Sum of odd elements :%d",iRet);
+
+
+    free(Brr);
+
+    return 0 ;
+}
