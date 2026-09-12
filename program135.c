1e5d727088a5ebb33da0eddffa6c39dd72985a03
Ritesh Gatane <254677979+riteshgatane@users.noreply.github.com>
2026-09-12 07:02:21 +0530

    Add files via upload

---
+++ program135.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+// Searching the maximum Number
+int Maximum(int Arr[], int iSize  )
+{
+    int iCnt = 0;
+    int iMax = 0;
+
+    iMax = Arr[0];
+
+    for (iCnt = 0 ; iCnt < iSize ; iCnt ++)
+   { 
+    if((Arr[iCnt]) > iMax )
+    {
+        iMax = Arr[iCnt]; 
+    }
+   }
+   return iMax ;
+    
+}
+
+int main()
+{
+    int *Brr = NULL ;
+    int iLength = 0,iCnt = 0  ;
+    int iRet = 0 ;
+    
+    printf("Enter the Number of elements:");
+    scanf("%d",&iLength);
+  
+    Brr = (int *)malloc(iLength * sizeof(int ));
+
+  
+    printf("Enter the elements:\n");
+    for (iCnt = 0 ; iCnt < iLength ; iCnt ++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+
+    iRet = Maximum(Brr , iLength  );
+    printf("Maximum element is %d",iRet);
+    
+    free(Brr);
+
+    return 0 ;
+}
+
+
+
