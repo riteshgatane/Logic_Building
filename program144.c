e27b4b3e352de59891c5e09920390b4897bb109d
Ritesh Gatane <254677979+riteshgatane@users.noreply.github.com>
2026-09-13 11:50:16 +0530

    Add files via upload

---
+++ program144.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void update(int Arr[] , int iSize)
+{
+    int iCnt =0 ;
+     for(iCnt =0 ; iCnt < iSize ;iCnt ++)
+    {
+       Arr[iCnt]++ ;
+    }
+}
+
+
+int main()
+{
+    int Brr[]={10,20,30,40,50};
+    int iCnt =0;
+    printf("Array elements before function call:\n");
+    
+    for(iCnt =0 ; iCnt < 5 ;iCnt ++)
+    {
+        printf("%d\n",Brr[iCnt]);
+    }
+
+    update(Brr , 5);
+    printf("Array elements After function call:\n");
+        for(iCnt =0 ; iCnt < 5 ;iCnt ++)
+    {
+        printf("%d\n",Brr[iCnt]);
+    }    
+    return 0 ;
+}
