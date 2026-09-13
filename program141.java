3b14075c80ed527dba44ea05b1b5fc49c5dfd31f
Ritesh Gatane <254677979+riteshgatane@users.noreply.github.com>
2026-09-13 11:45:43 +0530

    Add files via upload

---
+++ program141.java
@@ -0,0 +1,29 @@
+import  java.util.*;
+
+class program141
+{
+    public static void main (String A[])
+    { 
+        Scanner sobj = new Scanner(System.in);
+
+        int iCnt = 0;
+       
+        System.out.println("Enter the Number of Elements:");
+        int iLength = sobj.nextInt() ;
+
+        int Brr[] = new int[iLength]; 
+        System.out.println("Emter the elements:");
+
+        for(iCnt= 0 ; iCnt < Brr.length ; iCnt ++)
+        {
+            Brr[iCnt] = sobj.nextInt() ;
+        }  
+
+        System.out.println("Elements of Array are :");
+         for(iCnt= 0 ; iCnt < Brr.length ; iCnt ++)
+        {
+            System.out.println(Brr[iCnt]);
+        }  
+
+    }
+}
