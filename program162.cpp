58406844cf4efbad8f56d142b1ff0fcbbec0eec1
Ritesh Gatane <254677979+riteshgatane@users.noreply.github.com>
2026-09-17 01:10:03 +0530

    Add files via upload

---
+++ program162.cpp
@@ -0,0 +1,43 @@
+#include <iostream>   
+using namespace std ;
+#pragma pack(1)
+
+
+class ArrayX   
+{
+    public:
+    int *Arr  ;
+    int iSize ;
+
+
+    //Parameterized Constructor 
+    ArrayX(int X)
+    {
+        
+        iSize = X ;            //Characteristics Initialization
+        Arr = new int[iSize];  //Resource Allocation  
+    }
+
+    //Destructor 
+    ~ArrayX()
+    {
+    
+        delete []Arr ;    ///Resource Deallocation
+    }
+
+};
+
+int main()
+{
+      
+
+    ArrayX *aobj1 = new ArrayX(5);
+
+    cout << aobj1->iSize << "\n";
+
+    //Logic (Fucation Call)
+
+    delete aobj1 ;
+    
+    return 0;
+}
