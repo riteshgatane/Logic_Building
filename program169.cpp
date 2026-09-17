39c901756fc8cdb9ded61ee15ff723d8ef9b1766
Ritesh Gatane <254677979+riteshgatane@users.noreply.github.com>
2026-09-17 23:53:51 +0530

    Add files via upload

---
+++ program169.cpp
@@ -0,0 +1,81 @@
+#include <iostream>   
+using namespace std ;
+#pragma pack(1)
+
+
+class ArrayX   
+{
+    private:
+    int *Arr  ;
+    int iSize ;
+    int iCnt =0;
+    
+
+    public:  
+    //Parametrized Constructor with Default Argument  
+    ArrayX(int X = 5 )  //Default Argument
+    {
+        
+        iSize = X ;            
+        Arr = new int[iSize];  
+    }
+
+  
+    ~ArrayX()
+    {
+        delete []Arr ;  
+    } 
+
+    void Accept()
+    {
+        cout << "Enter the Elements: \n ";
+        for(iCnt = 0 ; iCnt < iSize ; iCnt++)
+        {
+            cin >> Arr[iCnt]; 
+        } 
+
+    }
+    
+     void Display()
+    {
+        cout << "Elements of  the Array are :\n";
+        for(iCnt = 0 ; iCnt < iSize ; iCnt++)
+        {
+            cout <<  Arr[iCnt] << "\n" ; 
+        } 
+
+    }
+
+    int Summation()
+    {
+        int iSum =0 ;
+        for(iCnt =0 ; iCnt < iSize ; iCnt ++)
+        {
+            iSum = iSum + Arr[iCnt];
+        }
+        return iSum ;
+        
+    }
+};
+
+int main()
+{
+   
+    ArrayX *aobj = NULL ;
+    int iLength = 0;
+    int iRet =0 ;
+
+    cout << "Enter the number of the eleements:";
+    cin >> iLength ;
+
+    aobj = new ArrayX(iLength);
+
+    aobj -> Accept();
+    aobj -> Display();
+
+    iRet = aobj -> Summation() ;
+    cout << "Summation is :" << iRet << endl ;
+
+    delete aobj ;
+    return 0;
+}
