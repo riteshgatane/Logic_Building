f572104e2701640832fd9d8b550be8889eafb9c9
Ritesh Gatane <254677979+riteshgatane@users.noreply.github.com>
2026-09-20 22:47:54 +0530

    Add files via upload

---
+++ program184.java
@@ -0,0 +1,47 @@
+ //input : 7
+ //output :    A  *  B   *  C   *    D    * 
+  
+
+import java.util.*;
+
+class program184  //
+{
+
+    public static void Dispaly(int iNo)
+    {
+        int iCnt = 0 ;
+        char ch ='\0' ;   //default value
+
+        for(iCnt =1 ,ch = 'A' ;iCnt <= iNo;iCnt++ )    
+        {
+           if((iCnt%2) == 0)
+            {
+            System.out.print("*\t");
+            }
+            else
+            {
+                System.out.print(ch+"\t");
+                ch++;
+            }
+        }
+
+        
+       
+
+
+
+    }
+    public static void main(String  A[] ) 
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue ;
+
+
+        System.out.println("Enter the number of elements:");
+        
+        iValue = sobj.nextInt();
+
+        Dispaly(iValue);
+               
+    }
+}
