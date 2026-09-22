fff252229098874338eac50f48cb0be37f66d249
Ritesh Gatane <254677979+riteshgatane@users.noreply.github.com>
2026-09-22 18:25:20 +0530

    Add files via upload

---
+++ program196.java
@@ -0,0 +1,52 @@
+/*
+    iROw =4
+    iCol =4
+
+    #   &   #   &
+    #   &   #   &
+    #   &   #   &
+    #   &   #   &
+
+
+*/
+import java.util.*;
+
+class program195  //
+{
+    public static void Dispaly(int iRow , int iCol)
+    {
+        int i,j = 0;
+
+       for(i = 1 ; i <= iRow ; i ++ )
+       {
+           { for(j = 1 ; j <= iCol ; j ++ )
+           if(( j% 2) == 0)
+                { 
+                    System.out.print("&\t");
+                }
+            else
+                {
+                    System.out.print("#\t");
+
+                }
+                System.out.println();
+            }
+       }
+    }
+      
+    public static void main(String  A[] ) 
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 ;
+        int iValue2 ;
+
+        System.out.println("Enter the number of Rows:");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of Columns:");
+        iValue2 = sobj.nextInt();
+
+        Dispaly(iValue1 ,iValue2);
+               
+    }
+}
