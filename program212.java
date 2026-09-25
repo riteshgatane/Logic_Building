ce399bdb189f65e054b201a8604389df1fe7d458
Ritesh Gatane <254677979+riteshgatane@users.noreply.github.com>
2026-09-25 11:07:08 +0530

    Add files via upload

---
+++ program212.java
@@ -0,0 +1,68 @@
+/*
+    iRow = 6
+    iCol = 6
+
+    #   #   #   #   #   #
+    @   @   @   @   @   @
+    @   @   @   @   @   @
+    @   @   @   @   @   @
+    @   @   @   @   @   @
+    #   #   #   #   #   #
+
+*/
+
+import java.util.* ;   ///
+
+class Pattern
+{
+    public void Display(int iRow ,int iCol)
+    {
+        int i = 0 ;
+        int j =0 ;
+        //filter for diagonal pattern
+        if(iRow != iCol)
+        {
+            System.out.println("Invalid parameter");
+            return; 
+        }
+      for(i = 1 ;i <= iRow ; i ++ )
+        {
+            
+            for(j = 1 ; j <= iCol ; j++ )
+            {
+                if(i == iRow || i == 1)
+                {
+                    System.out.print("#\t");
+                }
+                else
+                {
+                      System.out.print("@\t");
+                }
+                
+            }
+            System.out.println();
+        }
+         
+    }
+}
+
+
+class program212
+{
+    public static void main(String A[])
+    {
+        int iValue1 =0 ;
+        int iValue2 =0 ;
+
+        Scanner sobj = new Scanner(System.in);
+        System.out.println("Enter number of Rows");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of Columns");
+        iValue2 = sobj.nextInt() ;
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1 ,iValue2);
+    }
+}
